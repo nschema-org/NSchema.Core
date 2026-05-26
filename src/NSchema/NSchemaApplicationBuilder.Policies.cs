9d01575de3a11b719c1cf3f55df9cebcfc6335ab
Tom Wolfe <tom.wolfe@disastercare.co.uk>
2026-05-27 00:50:37 +0100

    feat: more configuration methods.

--- src/NSchema/NSchemaApplicationBuilder.Policies.cs
+++ src/NSchema/NSchemaApplicationBuilder.Policies.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using NSchema.Policies;
@@ -19,6 +20,29 @@ public partial class NSchemaApplicationBuilder
     }
 
     /// <summary>
+    /// Adds all concrete types that implement <see cref="ISchemaPolicy"/> to the application from the specified assembly.
+    /// </summary>
+    /// <param name="assembly">The assembly to scan for schema policies.</param>
+    /// <returns>The application builder, for chaining.</returns>
+    public NSchemaApplicationBuilder AddSchemaPoliciesFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetTypes()
+            .Where(t => t is { IsAbstract: false, IsInterface: false } && typeof(ISchemaPolicy).IsAssignableFrom(t));
+        foreach (var type in types)
+        {
+            Services.TryAddEnumerable(new ServiceDescriptor(typeof(ISchemaPolicy), type, ServiceLifetime.Singleton));
+        }
+        return this;
+    }
+
+    /// <summary>
+    /// Adds all concrete types that implement <see cref="ISchemaPolicy"/> to the application from the assembly containing the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type whose containing assembly will be scanned for schema policies.</typeparam>
+    /// <returns>The application builder, for chaining.</returns>
+    public NSchemaApplicationBuilder AddSchemaPoliciesFromAssemblyContaining<T>() => AddSchemaPoliciesFromAssembly(typeof(T).Assembly);
+
+    /// <summary>
     /// Adds a policy to the application that will be used to validate the generated migration plan before it is executed.
     /// </summary>
     /// <typeparam name="T">The type of the policy to add.</typeparam>
@@ -29,4 +53,27 @@ public partial class NSchemaApplicationBuilder
         Services.TryAddEnumerable(descriptor);
         return this;
     }
+
+    /// <summary>
+    /// Adds all concrete types that implement <see cref="IMigrationPolicy"/> to the application from the specified assembly.
+    /// </summary>
+    /// <param name="assembly">The assembly to scan for migration policies.</param>
+    /// <returns>The application builder, for chaining.</returns>
+    public NSchemaApplicationBuilder AddMigrationPoliciesFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetTypes()
+            .Where(t => t is { IsAbstract: false, IsInterface: false } && typeof(IMigrationPolicy).IsAssignableFrom(t));
+        foreach (var type in types)
+        {
+            Services.TryAddEnumerable(new ServiceDescriptor(typeof(IMigrationPolicy), type, ServiceLifetime.Singleton));
+        }
+        return this;
+    }
+
+    /// <summary>
+    /// Adds all concrete types that implement <see cref="IMigrationPolicy"/> to the application from the assembly containing the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type whose containing assembly will be scanned for migration policies.</typeparam>
+    /// <returns>The application builder, for chaining.</returns>
+    public NSchemaApplicationBuilder AddMigrationPoliciesFromAssemblyContaining<T>() => AddMigrationPoliciesFromAssembly(typeof(T).Assembly);
 }
--- src/NSchema/NSchemaApplicationBuilder.Transformers.cs
+++ src/NSchema/NSchemaApplicationBuilder.Transformers.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using NSchema.Migration;
@@ -17,4 +18,27 @@ public partial class NSchemaApplicationBuilder
         Services.TryAddEnumerable(descriptor);
         return this;
     }
+
+    /// <summary>
+    /// Adds all concrete types that implement <see cref="IMigrationPlanTransformer"/> to the application from the specified assembly.
+    /// </summary>
+    /// <param name="assembly">The assembly to scan for plan transformers.</param>
+    /// <returns>The application builder, for chaining.</returns>
+    public NSchemaApplicationBuilder AddPlanTransformersFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetTypes()
+            .Where(t => t is { IsAbstract: false, IsInterface: false } && typeof(IMigrationPlanTransformer).IsAssignableFrom(t));
+        foreach (var type in types)
+        {
+            Services.TryAddEnumerable(new ServiceDescriptor(typeof(IMigrationPlanTransformer), type, ServiceLifetime.Singleton));
+        }
+        return this;
+    }
+
+    /// <summary>
+    /// Adds all concrete types that implement <see cref="IMigrationPlanTransformer"/> to the application from the assembly containing the specified type.
+    /// </summary>
+    /// <typeparam name="T">The type whose containing assembly will be scanned for plan transformers.</typeparam>
+    /// <returns>The application builder, for chaining.</returns>
+    public NSchemaApplicationBuilder AddPlanTransformersFromAssemblyContaining<T>() => AddPlanTransformersFromAssembly(typeof(T).Assembly);
 }
