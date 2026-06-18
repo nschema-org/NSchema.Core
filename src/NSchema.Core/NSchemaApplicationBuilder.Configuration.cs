9a62988946ed59acf1d25b600109fead19a9e683
Tom Wolfe <trwolfe13@gmail.com>
2026-06-18 02:05:06 +0100

    chore: combine some config.

--- src/NSchema.Core/NSchemaApplicationBuilder.Configuration.cs
+++ src/NSchema.Core/NSchemaApplicationBuilder.Configuration.cs
@@ -82,4 +82,14 @@ public partial class NSchemaApplicationBuilder
         Services.Replace(ServiceDescriptor.Singleton(reporter));
         return this;
     }
+
+    /// <summary>
+    /// Sets the <see cref="ISqlGenerator"/> the application generates SQL with, replacing any previously set one.
+    /// Typically called by a database-provider extension. With none set, plans are reported without a SQL preview.
+    /// </summary>
+    public NSchemaApplicationBuilder UseSqlGenerator<T>() where T : class, ISqlGenerator
+    {
+        Services.Replace(ServiceDescriptor.Singleton<ISqlGenerator, T>());
+        return this;
+    }
 }
